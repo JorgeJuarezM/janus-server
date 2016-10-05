4581d8bc4117e9cf31487992fd6986260383fd69
Jorge Juarez <contacto@jorgejuarez.net>
2016-10-05 09:19:38 -0500

    Remove Test

--- test.js
+++ test.js
@@ -34,14 +34,13 @@ var async__ = require("asyncawait/async");
 var await__ = require("asyncawait/await");
 
 var cnn  = janusServer.getConnection("test"); //DbConnection
-var tran = new janusServer.data.DbTransaction(cnn);
+var tran = new janusServer.data.DbTransaction(cnn); //DbTransaction
 
 
 cnn.connect(function () {
     console.log("Connected");
     var cmd = new janusServer.data.DbCommand.Select("T1", tran);
-    // cmd.limit(1);
-
+    
     cmd.query = "INSERT INTO T1 (id) values (1)";
 
     tran.begin(function () {
